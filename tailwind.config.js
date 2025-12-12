194651d10b833302cb7fcc35390947262889adb1
RavexCode <rd77extragd@gmail.com>
2025-12-12 13:46:45 -0600

    Bugfix y subida de imagenes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,37 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+// tailwind.config.js
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        updatable: {
+          "0%":   { backgroundImage: "url('/cbtis258_1.webp')" },
+          "10%":  { backgroundImage: "url('/cbtis258_2.webp')" },
+          "15%":  { backgroundImage: "url('/cbtis258_3.webp')" },
+          "21%":  { backgroundImage: "url('/cbtis258_4.webp')" },
+          "26%":  { backgroundImage: "url('/cbtis258_5.webp')" },
+          "31%":  { backgroundImage: "url('/cbtis258_6.webp')" },
+          "36%":  { backgroundImage: "url('/cbtis258_7.webp')" },
+          "42%":  { backgroundImage: "url('/cbtis258_8.webp')" },
+          "52%":  { backgroundImage: "url('/cbtis258_10.webp')" },
+          "57%":  { backgroundImage: "url('/cbtis258_11.webp')" },
+          "63%":  { backgroundImage: "url('/cbtis258_12.webp')" },
+          "68%":  { backgroundImage: "url('/cbtis258_13.webp')" },
+          "73%":  { backgroundImage: "url('/cbtis258_14.webp')" },
+          "79%":  { backgroundImage: "url('/cbtis258_15.webp')" },
+          "84%":  { backgroundImage: "url('/cbtis258_16.webp')" },
+          "95%":  { backgroundImage: "url('/cbtis258_18.webp')" },
+          "100%": { backgroundImage: "url('/cbtis258_19.webp')" },
+        }
+      },
+
+      animation: {
+        updatable: "updatable 150s ease-in-out infinite",
+      },
+    },
   },
   plugins: [],
 };
